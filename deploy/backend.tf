948ba0f8d2aea81872f3c91ddd1f66b939786db7
mjafanya <162713872+mjafanya@users.noreply.github.com>
2024-05-26 22:05:02 +0530

    Update backend.tf

--- deploy/backend.tf
+++ deploy/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket  = "kunduso-terraform-remote-bucket"
+    bucket  = "ecs-bucket77"
     encrypt = true
     key     = "tf/add-aws-ecr-ecs-fargate/deploy-ecs.tfstate"
     region  = "us-east-2"
